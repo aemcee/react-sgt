417e4fc803b63d4bf76e46410bba1129097d2ee8
Anthony Capili <acapili07@gmail.com>
2019-04-20 12:47:10 -0700

    display-students completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
 import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import Table from './table';
+import AddStudent from './add_student';
+import {Route, Link} from 'react-router-dom';
+
+
+// install react router dom
+// setup routing
+// create 2 class components
+// add student and table
+// setup route for table to be displayed on home route /
+// addStudent to be displayed on /add-student
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
-        </div>
+    <div className="container">
+        {/* <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/addstudent">Add Student</Link>
+            </li>
+        </ul> */}
+         <Link to="/">Home</Link>
+         <Link to="/addstudent">Add Student</Link>
+        <Route exact path="/" component={Table}></Route>
+        <Route path="/addstudent" component={AddStudent}></Route>
     </div>
 );
 
