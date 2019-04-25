6a698e9c15c3c9d6eb6fd20ab0fc37b5bdbe9a40
Anthony Capili <acapili07@gmail.com>
2019-04-24 19:10:48 -0700

    completed add student and nav link

--- src/components/add_student.js
+++ src/components/add_student.js
@@ -1,9 +1,113 @@
 import React, {Component} from 'react';
+import Input from './input';
+import NavLink from './nav_link';
+
+// Buld a form for new students with the following inputs:
+// - Name, Course, Grade,
+// When submitted console.logs the new student info
 
 class AddStudent extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            form: {
+                name: '',
+                course: '',
+                grade: ''
+            }
+        }
+
+    }
+
+    reset = () => {
+        this.setState({
+            form: {
+                name: '',
+                course: '',
+                grade: ''
+            }
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submit Registered',this.state.form);
+
+        // prevent mutating the state
+        this.props.add({...this.state.form});
+
+        // can use to redirect without clicking a link???
+        this.props.history.push('/');
+        this.reset();
+    }
+
+    handleInputChange = (event) => {
+        const {form} = this.state;
+        const {value,name} = event.target;
+        console.log('event.target',event.target.value);
+        console.log('typeof(value)',typeof(value));
+        // console.log('form1',form);
+        // console.log('this.state1',this.state.form);
+       
+        form[name]=value;
+        // console.log('form[name]',form[name]);
+        // console.log('{...form}',{...form});
+        // console.log('form2',form);
+        // console.log('this.state2',this.state.form);
+        this.setState({
+            form: {...form}
+        });
+    }
+
+    renderForm(){
+
+        const {name,course,grade} = this.state.form;
+
+        // console.log('student',student);
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="row">
+                    {/* <div className="input-field col s12">
+                        <input onChange={this.handleInputChange} value={name} name="name" type="text" autoComplete="off" id="std"></input>
+                        <label htmlFor="std">Input Student</label>
+                    </div> */}
+                    {/* <Input onChange={this.handleInputChange} value={student} name={`${props.form}`} type="text" autoComplete="off" id="std"></Input> */}
+                    <Input onChange={this.handleInputChange} value={name} name="name" type="text" autoComplete="off" id="std" label="Input Student"></Input>
+                </div>
+                <div className="row">
+                    {/* <div className="input-field col s12">
+                        
+                        <input onChange={this.handleInputChange} value={course} name="course" type="text" autoComplete="off" id="crse"></input>
+                        <label htmlFor="crse">Input Course</label>
+                    </div> */}
+                    <Input onChange={this.handleInputChange} value={course} name="course" type="text" autoComplete="off" id="crse" label="Input Course"></Input>
+                </div>
+                <div className="row">
+                    {/* <div className="input-field col s12">
+                    
+                        <input onChange={this.handleInputChange} value={grade} name="grade" type="text" autoComplete="off" id="grd"></input>
+                        <label htmlFor="grd">Input Grade</label>
+                    </div> */}
+                    <Input onChange={this.handleInputChange} value={grade} name="grade" type="number" autoComplete="off" id="grd" label="Input Grade"></Input>
+                </div>
+                <div>
+                    <button className="btn">Add Student</button>
+                    <button className="btn" type="button" onClick={this.reset}>Clear Form</button>
+                </div>
+            </form>
+        )
+    }
+
     render(){
         return (
-            <h1>Add Student test</h1>
+            <div className="center">
+                <h1>Add Student</h1>
+                <NavLink color="blue darken-2" text="Student Table" to="/"></NavLink>
+                {this.renderForm()}
+            </div>
         )
     }
 }
